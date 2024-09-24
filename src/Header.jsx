73b78a8aeb821cdaf1b50284af7487894e173a9f
NuskheladzeShota <nuskheladzeshota@gmail.com>
2024-09-24 13:15:42 +0400

    create header

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header(){
+    return(
+        <div className="">
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>       
+        </div>
+    )
+}
+
