10432b481fe5b72f017fa465bc86c7b77324281f
NuskheladzeShota <nuskheladzeshota@gmail.com>
2024-10-06 22:36:25 +0400

    create profile

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Contact from './Pages/Contact';
 import About from './Pages/About';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Assignment from './Pages/Assignment';
+import Profile from './Pages/Profile';
 function App() {
   return (
     <Router>
@@ -12,6 +13,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} />
           <Route path="/assignment" element={<Assignment />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </div>
     </Router>
